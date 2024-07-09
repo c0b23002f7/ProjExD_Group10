7d53ea0ef67c2c687878fd542af7e3464616280b
shingo <c0b23002f7@edu.teu.ac.jp>
2024-07-09 18:44:54 +0900

    途中2Hiroinn1

--- Dreamer.py
+++ Dreamer.py
@@ -9,14 +9,14 @@ class Hiroin:
     ヒロインに関するクラス
     """
     delta = {  # 押下キーと移動量の辞書
-        pg.K_LEFT: (-1, 0),
-        pg.K_RIGHT: (+1, 0),
+        pg.K_LEFT: (-10, 0),
+        pg.K_RIGHT: (+10, 0),
     }
 
     imgs = {
         (+5, 0): pg.image.load("fig/file.png"),  # 初期画像を指定してください
-        (-1, 0): pg.image.load("fig/file(0).png"),
-        (+1, 0): pg.image.load("fig/file(1).png")
+        (-10, 0): pg.image.load("fig/file(0).png"),
+        (+10, 0): pg.image.load("fig/file(1).png")
     }
 
     
@@ -29,9 +29,9 @@ class Hiroin:
         self.img = __class__.imgs[(+5, 0)]
         self.rct: pg.Rect = self.img.get_rect()
         self.rct.center = xy
-        self.gravity = 0.5
+        self.gravity = 1
         self.is_jumping = False
-        self.jump_speed = 20
+        self.jump_speed = 15
     def change_img(self, num: int, screen: pg.Surface):
         self.image = pg.transform.rotozoom(pg.image.load(f"fig/file({num}).png"), 0, 2.0)
         self.rect = self.image.get_rect()
@@ -68,10 +68,6 @@ def main():
     hl_img = Hiroin((100, 630))
 
     show_hl_img = False
-    # hl_img = pg.image.load("file.png")
-    # hl_img = pg.transform.flip(hl_img, True, False)
-    # kk_rect = hl_img.get_rect() #こうかとんrectの抽出
-    # kk_rect.center = 300, 200
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -84,35 +80,9 @@ def main():
         key_lst = pg.key.get_pressed()
         if show_hl_img:
             hl_img.update(key_lst, screen)
-        #     if event.type == pg.QUIT: return
-
-        # x = tmr%3200
-        # screen.blit(bg_img, [-x, 0]) #背景画像を表すsurfase
-        # screen.blit(bg_img2, [-x+1600, 0])
-        # screen.blit(bg_img, [-x+3200, 0]) #背景画像を表すsurfase
-        # screen.blit(bg_img2, [-x+4800, 0])
-        
-        # kye_lst = pg.key.get_pressed()
-        # if kye_lst[pg.K_UP]: #上矢印を押したとき
-        #     a = -1
-        #     b = -1
-        # elif kye_lst[pg.K_DOWN]:
-        #     a = -1
-        #     b = +1
-        # elif kye_lst[pg.K_LEFT]:
-        #     a = -1
-        #     b = 0
-        # elif kye_lst[pg.K_RIGHT]:
-        #     a = +2
-        #     b = 0
-        # else:
-        #     a = -1
-        #     b = 0
-        # kk_rect.move_ip(a, b)
-        # screen.blit(hl_img, kk_rect) #kk_imageをkk_rectの設定に従って貼り付け
         pg.display.update()
         # tmr += 1        
-        clock.tick(200)
+        clock.tick(60)
 
 
 if __name__ == "__main__":
